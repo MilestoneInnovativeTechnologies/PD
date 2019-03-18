66d92085dcb5d6271d2bb9c3c73d7fdcb6d6abe7
Firose Hussain <github@firumon.com>
2019-03-18 12:41:33 +0530

    Made properties as public static to make it access from other controllers

--- src/Controller/API/ProductController.php
+++ src/Controller/API/ProductController.php
@@ -8,15 +8,15 @@ use Milestone\PD\Model\ProductField;
 
 class ProductController extends Controller
 {
-    static $SearchFields = ['description'], $detailFields = ['id','name','description'];
-    public $relationsGroup = ['Group01','Group02','Group03','Group04','Group05','Group06','Group07','Group08','Group09','Group10'];
+    public static $SearchFields = ['description'], $detailFields = ['id','name','description'];
+    public static $relationsGroup = ['Group01','Group02','Group03','Group04','Group05','Group06','Group07','Group08','Group09','Group10'];
 
     static function ProductsQuery(){
         $self = new self;
         $fields = array_merge($self::$detailFields,ProductField::pluck('field_name')->toArray());
-        $groups = array_intersect(array_map('strtolower',$self->relationsGroup),$fields);
+        $groups = array_intersect(array_map('strtolower',$self::$relationsGroup),$fields);
         $Query = Product::select($fields)->with($groups)->where(['type' => 'Public','status' => 'Active']);
-        foreach($self->relationsGroup as $g) {
+        foreach($self::$relationsGroup as $g) {
             $Query->where(function($Q)use($g,$groups){
                 if(in_array(strtolower($g),$groups)) $Q->whereNull(strtolower($g))->orWhereHas($g,function($Q){ $Q->web(); });
             });
@@ -27,7 +27,7 @@ class ProductController extends Controller
     public function detail(Request $request){
         if(is_null($request->get('id'))) return $this->noProductResponse();
         $fields = ProductField::pluck('field_name')->toArray();
-        $groups = array_intersect(array_map('strtolower',$this->relationsGroup),$fields);
+        $groups = array_intersect(array_map('strtolower',$this::$relationsGroup),$fields);
         return self::product($request->get('id'),$fields,$groups) ?: $this->noProductResponse();
     }
 
